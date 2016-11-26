bbb91b633e04a0d5d060bb1e0793a41fae6c6a43
Leonard Bogdonoff <rememberlenny@gmail.com>
2016-11-25 20:45:42 -0500

    Set up the test stubs

--- spec/robinhood_spec.rb
+++ spec/robinhood_spec.rb
@@ -5,7 +5,25 @@ describe Robinhood do
     expect(Robinhood::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  describe "setup the robinhood object and" do
+    it "fail without config params" do
+      pending
+    end
+
+    it "access a username and password param" do
+      pending
+    end
+
+    it "get the access token from the api" do
+      pending
+    end
+
+    it "store the access token as a local variable" do
+      pending
+    end
+  end
+
+  it "make sure tests are working" do
     expect(true).to eq(true)
   end
 end
