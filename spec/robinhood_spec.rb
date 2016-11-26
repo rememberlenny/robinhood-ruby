2f5a6257c9a24c45f806debb5e49a0b9693e74c8
Leonard Bogdonoff <rememberlenny@gmail.com>
2016-11-25 20:55:18 -0500

    Create the auth check

--- spec/robinhood_spec.rb
+++ spec/robinhood_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Robinhood do
   let(:username) {"username"}
   let(:password) {"password"}
-  
+
   it "has a version number" do
     expect(Robinhood::VERSION).not_to be nil
   end
@@ -14,14 +14,18 @@ describe Robinhood do
     end
 
     it "access a username and password param" do
-      pending
+      @robinhood = Robinhood::REST::Client.new username, password 
+      expect(@robinhood.username).to be_a String
+      expect(@robinhood.username).to eq username
+      expect(@robinhood.password).to be_a String
+      expect(@robinhood.password).to eq password
     end
 
     it "get the access token from the api" do
       FakeWeb.register_uri(:get, "https://api.robinhood.com/api-token-auth/", :body => {"token"=>"1234567890"})
       @robinhood = Robinhood::REST::Client.new username, password 
-      expect(@token).to be_a String
-      expect(@token).to eq '1234567890'
+      expect(@robinhood.token).to be_a String
+      expect(@robinhood.token).to eq '1234567890'
     end
 
     it "store the access token as a local variable" do
