8479eaeb488b46376bc4cc2e811ce34bca27bf0b
Leonard Bogdonoff <rememberlenny@gmail.com>
2016-11-25 21:57:28 -0500

    Move the config down

--- spec/robinhood_spec.rb
+++ spec/robinhood_spec.rb
@@ -8,26 +8,6 @@ describe Robinhood do
     expect(Robinhood::VERSION).not_to be nil
   end
 
-  describe "setup the Robinhood::Utils::ClientConfig" do
-    Robinhood::Util::ClientConfig::DEFAULTS.each do |attribute, value|
-      it "sets and attribune with a " + attribute.to_s + " value" do
-        config = Robinhood::Util::ClientConfig.new
-        expect(config.send(attribute)).to eq(value)
-      end
-
-      it "can update the value for the attribute" do
-        config = Robinhood::Util::ClientConfig.new
-        config.send("#{attribute}=", "blah")
-        expect(config.send(attribute)).to eq("blah")
-      end
-
-      it "can set the value from a hash in the initializer" do
-        config = Robinhood::Util::ClientConfig.new(attribute => 'blah')
-        expect(config.send(attribute)).to eq("blah")
-      end
-    end
-  end
-
   describe "setup the Robinhood::REST::Client" do
     it "fail without config params" do
       expect{ Robinhood::REST::Client.new }.to raise_error(ArgumentError)
@@ -46,6 +26,27 @@ describe Robinhood do
       @robinhood = Robinhood::REST::Client.new username, password 
       expect(@robinhood.token).to be_a String
       expect(@robinhood.token).to eq '1234567890'
+    end
+  end
+
+
+  describe "setup the Robinhood::Utils::ClientConfig" do
+    Robinhood::Util::ClientConfig::DEFAULTS.each do |attribute, value|
+      it "sets and attribune with a " + attribute.to_s + " value" do
+        config = Robinhood::Util::ClientConfig.new
+        expect(config.send(attribute)).to eq(value)
+      end
+
+      it "can update the value for the attribute" do
+        config = Robinhood::Util::ClientConfig.new
+        config.send("#{attribute}=", "blah")
+        expect(config.send(attribute)).to eq("blah")
+      end
+
+      it "can set the value from a hash in the initializer" do
+        config = Robinhood::Util::ClientConfig.new(attribute => 'blah')
+        expect(config.send(attribute)).to eq("blah")
+      end
     end
   end
 
