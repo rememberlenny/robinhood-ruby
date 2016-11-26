3c82aa9a763add77c3e1e5b53055038f7b19c80b
Leonard Bogdonoff <rememberlenny@gmail.com>
2016-11-25 22:34:54 -0500

    Fix the client config

--- spec/robinhood_spec.rb
+++ spec/robinhood_spec.rb
@@ -4,14 +4,16 @@ describe Robinhood do
   it "has a version number" do
     expect(Robinhood::VERSION).not_to be nil
   end
-
+  
+  let(:username) {"username"}
+  let(:password) {"password"}
   describe "setup the Robinhood::REST::Client" do
     it "fail without config params" do
       expect{ Robinhood::REST::Client.new }.to raise_error(ArgumentError)
     end
 
     it "access a username and password param" do
-      @robinhood = Robinhood::REST::Client.new username, password 
+      @robinhood = Robinhood::REST::Client.new(username, password)
       expect(@robinhood.username).to be_a String
       expect(@robinhood.username).to eq username
       expect(@robinhood.password).to be_a String
@@ -20,24 +22,21 @@ describe Robinhood do
 
     it "get the access token from the api and store the access token as a local variable" do
       FakeWeb.register_uri(:get, "https://api.robinhood.com/api-token-auth/", :body => {"token"=>"1234567890"})
-      @robinhood = Robinhood::REST::Client.new username, password 
+      @robinhood = Robinhood::REST::Client.new(username, password)
       expect(@robinhood.token).to be_a String
       expect(@robinhood.token).to eq '1234567890'
     end
   end
 
-
-  let(:username) {"username"}
-  let(:password) {"password"}
   describe "setup the Robinhood::Utils::ClientConfig" do
     Robinhood::Util::ClientConfig::DEFAULTS.each do |attribute, value|
       it "sets and attribune with a " + attribute.to_s + " value" do
-        config = Robinhood::Util::ClientConfig.new(username, password)
+        config = Robinhood::Util::ClientConfig.new
         expect(config.send(attribute)).to eq(value)
       end
 
       it "can update the value for the attribute" do
-        config = Robinhood::Util::ClientConfig.new(username, password)
+        config = Robinhood::Util::ClientConfig.new
         config.send("#{attribute}=", "blah")
         expect(config.send(attribute)).to eq("blah")
       end
