773e11d0b11a5b84cd77ba90775f5e8a8a2df49d
Leonard Bogdonoff <rememberlenny@gmail.com>
2016-12-09 14:27:54 -0500

    Example update

--- example/robinhood-sample/config/initializers/robinhood.rb
+++ example/robinhood-sample/config/initializers/robinhood.rb
@@ -1,1 +1,1 @@
-# @robinhood = Robinhood::REST::Client.new(ENV['robinhood_username'], ENV['robinhood_password'])
+@robinhood = Robinhood::REST::Client.new(ENV['robinhood_username'], ENV['robinhood_password'])
