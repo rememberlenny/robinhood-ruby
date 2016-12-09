f603094e3d9768b30e6edb119b3cac99231b49e3
Leonard Bogdonoff <rememberlenny@gmail.com>
2016-12-09 02:27:54 -0500

    Add the ability to buy orders

--- lib/robinhood/rest/api.rb
+++ lib/robinhood/rest/api.rb
@@ -59,42 +59,60 @@ module Robinhood
       end
 
       # def cancel_order(order)
-      #   if order[:cancel]
-      #     url = URI(@api_url + @endpoints[:cancel])
-      #   else
-      #   end
-      #   response = http_request(url)
-      #   puts response.read_body
-        # puts response.read_body
-      # end
+      #   if(order.cancel){
+      #     return _request.post({
+      #       uri: _apiUrl + order.cancel
+      #     }, callback);
+      #   }else{
+      #     callback({message: order.state=="cancelled" ? "Order already cancelled." : "Order cannot be cancelled.", order: order }, null, null);
+      #   };
+      # }
 
-      # def place_order(options, callback)
-      # return _request.post({
-      #   uri: @api_url + @endpoints.orders,
-      #   form: {
-      #     account: _private.account,
-      #     instrument: options.instrument.url,
-      #     price: options.bid_price,
-      #     stop_price: options.stop_price,
-      #     quantity: options.quantity,
-      #     side: options.transaction,
-      #     symbol: options.instrument.symbol.toUpperCase(),
-      #     time_in_force: options.time || 'gfd',
-      #     trigger: options.trigger || 'immediate',
-      #     type: options.type || 'market'
-      #   }
-      # }, callback);
-      # end
+      def place_order(options)
+        url = URI(@api_url + @endpoints[:orders])
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      # def place_buy_order(options, callback)
-      # options.transaction = 'buy';
-      # return _place_order(options, callback);
-      # end
+        form = {
+          account: _private.account,
+          instrument: options.instrument.url,
+          price: options.bid_price,
+          stop_price: options.stop_price,
+          quantity: options.quantity,
+          side: options.transaction,
+          symbol: options.instrument.symbol.toUpperCase(),
+          time_in_force: options.time || 'gfd',
+          trigger: options.trigger || 'immediate',
+          type: options.type || 'market'
+        }
 
-      # def place_sell_order(options, callback)
-      # options.transaction = 'sell';
-      # return _place_order(options, callback);
-      # end      
+        request = Net::HTTP::Post.new(url)
+        request["accept"] = '*/*'
+        request["accept-encoding"] = 'gzip, deflate'
+        request["accept-language"] = 'en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5'
+        request["content-type"] = 'multipart/form-data; boundary=---011000010111000001101001'
+        request["x-robinhood-api-version"] = '1.0.0'
+        request["connection"] = 'keep-alive'
+        request["user-agent"] = 'Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)'
+        request["authorization"] = 'Token ' + @private[:auth_token]
+        request["cache-control"] = 'no-cache'
+        request.body = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"account\"\r\n\r\n"+form[:account]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"instrument\"\r\n\r\n"+form[:instrument]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n"+form[:price]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"stop_price\"\r\n\r\n"+form[:stop_price]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"quantity\"\r\n\r\n"+form[:quantity]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"side\"\r\n\r\n"+form[:side]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"symbol\"\r\n\r\n"+form[:symbol]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"time_in_force\"\r\n\r\n"+form[:time_in_force]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"trigger\"\r\n\r\n"+form[:trigger]+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n"+form[:type]+"\r\n-----011000010111000001101001--"
+
+        response = http.request(request)
+        puts response.read_body
+        JSON.parse(response.read_body)
+      end
+
+      def place_buy_order(options)
+        options[:transaction] = 'buy'
+        place_order(options)
+      end
+
+      def place_sell_order(options)
+        options[:transaction] = 'sell'
+        place_order(options)
+      end      
 
       def positions
         url = URI(@api_url + @endpoints[:positions])
